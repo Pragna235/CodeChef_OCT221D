34a3e601928842699f3b90ab3b35402cd1e12e1e
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-10-08 23:30:03 +0530

    Add files via upload

---
+++ SINGLEOP2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,c=0;
+        cin>>n;
+        string s;
+        cin>>s;
+        for(i=0;i<n;i++)
+        {
+            c++;
+            if(s[i+1]=='1')
+               break;
+        }
+        cout<<c<<endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
